f7b70a2db61823f948769fa3adc12f07362ee4db
JavierLopez82 <javier.lopez@peppergroup.es>
2020-09-09 11:47:54 +0200

    Maximum-dron-cannons: Se obtienen las cumbres del array de entrada.

--- resistance-dron/Dron.cs
+++ resistance-dron/Dron.cs
@@ -11,9 +11,22 @@ namespace resistance_dron
         public int maxNumberLaserCannons(int[] A)
         {
             int numberConnons = 0;
+            int length = A.Length;
+            List<int> indexTops = new List<int>();
+
+            if (A != null && length > 0)
+            {
+                //Se obtienen las cumbres
+                for (int i = 1; i < (length - 1); i++)
+                {
+                    if (A[i] > A[i - 1] && A[i] > A[i + 1])
+                    {
+                        indexTops.Add(i);
+                    }
+                }
 
 
-
+            }
 
             return numberConnons;
         }
